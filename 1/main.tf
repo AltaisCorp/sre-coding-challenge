f0e69fdb52282adb9c157c3f115b1ef34ab8c368
Sebastian Londono <lallamavioleta@gmail.com>
2021-12-21 16:36:37 -0500

    added 1 complete

--- 1/main.tf
+++ 1/main.tf
@@ -1,6 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "wild-altair"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
 resource "aws_s3_bucket_object" "object" {
-  bucket = "your_bucket_name"
-  key    = "new_object_key"
+  bucket = aws_s3_bucket.b.id
+  key    = "file.txt"
   source = "file.txt"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
